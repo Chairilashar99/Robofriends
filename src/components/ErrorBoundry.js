105a8b7503a03f7d40389b5a6d6afc7a82723324
chairil ashar <asharchairil4@gmail.com>
2022-12-18 18:05:17 +0700

    hasdone

--- src/components/ErrorBoundry.js
+++ src/components/ErrorBoundry.js
@@ -1,6 +1,6 @@
-import React, {Componenet} from "react";
+import React, {Component} from "react";
 
-class ErrorBoundry extends Componenet{
+class ErrorBoundry extends Component{
     constructor(props){
         super(props);
         this.state = {
